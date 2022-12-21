bbdfff5a50f77f4449ac530512ff65911afaa359
bipul077 <bipulratna07@gmail.com>
2022-12-21 17:55:43 +0545

    video 22 done

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -24,7 +24,19 @@ function App() {
       setAlert(null);
     }, 2000);
   }
-  const toggleMode = ()=>{
+
+  const removebodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-primary')
+  }
+
+  const toggleMode = (cls)=>{
+    console.log(cls)
+    removebodyClasses()
+    document.body.classList.add('bg-'+cls)
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
