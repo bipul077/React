3904a9bed6650075821b7a43dfc9cf66f8c50b70
bipul077 <bipulratna07@gmail.com>
2022-12-26 14:15:25 +0545

    upto video 39

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -1,80 +1,118 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
-    articles = [{
-      "source": {
-          "id": "bbc-sport",
-          "name": "BBC Sport"
-      },
-      "author": null,
-      "title": "The numbers behind England's incredible year of Test cricket",
-      "description": "TMS statistician Andy Zaltzman assesses the numbers behind England's incredible year of Test cricket.",
-      "url": "http://www.bbc.co.uk/sport/cricket/64071323",
-      "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/A000/production/_128106904_stokesindex.jpg",
-      "publishedAt": "2022-12-23T07:52:27.1631722Z",
-      "content": "Harry Brook has joined an elite list of players with three or more hundreds in their first four Tests\r\nIn the curious vernacular of sports punditry, it is often said a team or individual which has pe… [+8147 chars]"
-  },
-  {
-      "source": {
-          "id": "news-com-au",
-          "name": "News.com.au"
-      },
-      "author": null,
-      "title": "BBL player abruptly sacked over incident",
-      "description": "<p>Up-and-coming fast bowler Fazalhaq Farooqi has had his Big Bash contract scrapped after a reported incident prompted action from Cricket NSW.<br></p>",
-      "url": "https://www.news.com.au/sport/cricket/big-bash/bbl12-sydney-thunder-recruit-has-contract-terminated-over-reported-incident/news-story/3c19e3736c718324f427e62e5ad07e78",
-      "urlToImage": "https://content.api.news/v3/images/bin/6962a6a172894cabc4bcb0fc75ea7744",
-      "publishedAt": "2022-12-23T05:25:00Z",
-      "content": "Up-and-coming fast bowler Fazalhaq Farooqi has had his Big Bash contract scrapped after a reported incident prompted action from Cricket NSW.\r\nThe 22-year-old is one of the many young Afghanistan pla… [+1781 chars]"
-  },
-  {
-      "source": {
-          "id": "espn-cric-info",
-          "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-  },
-  {
-      "source": {
-          "id": "espn-cric-info",
-          "name": "ESPN Cric Info"
-      },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+  static defaultProps={
+    country:'in',
+    pageSize: 8,
+    category: 'general',
   }
-  ]
-  constructor(){
-    super();
+    static propTypes={
+      country: PropTypes.string,
+      pageSize: PropTypes.number,
+      category: PropTypes.string,
+    }
+  
+  capitalizeFirstLetter = (string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props){
+    super(props);
+    console.log(this.props.apikey);
     console.log("hello i am a constructor from news component");
     this.state={
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: true,
+      page:1,
+      totalResults: 0
     }
+    document.title=`${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
+
+  async updateNews(){
+    this.props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    this.props.setProgress(30);
+    let parsedData = await data.json()
+    this.props.setProgress(70);
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles,
+      totalResults:parsedData.totalResults,  // parsedData.totalResults is from api's totalResults
+      loading: false
+    })
+    this.props.setProgress(100);
+  }
+
+
+  async componentDidMount(){
+    console.log("cdm");
+    this.updateNews();
+  }
+
+  fetchMoreData = async () => {
+    this.setState({page:this.state.page+1});
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults:parsedData.totalResults,  // parsedData.totalResults is from api's totalResults
+      loading: false
+    })
+  }
+
+  // handlePreviousClick = async ()=>{
+  //   console.log("prev");
+  //   this.setState({page:this.state.page-1});
+  //   this.updateNews();
+  // }
+
+  // handleNextClick = async ()=>{
+  //   console.log("next");
+  //   this.setState({page:this.state.page+1})
+  //   this.updateNews();
+  // }
+
   render() {
+    console.log("good")
     return (
-      <div className="container my-3">
-            <h2>This is a news component</h2>           
-            <div className="row">
-            {this.state.articles.map((element)=>{
-              return <div className="col-md-4" key={element.url}>
-                      <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,84)} imageUrl={element.urlToImage} newsUrl={element.url}/>
-              </div> 
-            })}
-      </div>
-      </div>
+      <>
+            <h2 className='text-center'>24*7 News on {this.capitalizeFirstLetter(this.props.category)}</h2> 
+            {this.state.loading && <Spinner/>}          
+            <InfiniteScroll
+              dataLength={this.state.articles.length}
+              next={this.fetchMoreData}
+              //style={{ display: 'flex', flexDirection: 'column-reverse' }} //To put endMessage and loader to the top.
+              hasMore={this.state.articles.length < this.state.totalResults}//loader wont show if hasmore becomes false
+              loader={<Spinner/>}
+            >
+            <div className="container">
+              <div className="row">
+              {/* {!this.state.loading && this.state.articles.map((element)=>{
+                return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title.slice(0,70):""} description={element.description?element.description.slice(0,84):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} published={element.publishedAt}/>
+                </div> 
+              })} */}
+              {this.state.articles.map((element)=>{
+                return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title.slice(0,70):""} description={element.description?element.description.slice(0,84):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} published={element.publishedAt}/>
+                </div> 
+                    })}
+              </div>
+            </div>
+      </InfiniteScroll>
+      {/* <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className='btn btn-dark' onClick={this.handlePreviousClick}>&larr; Previous</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+      </div> */}
+      </>
     )
   }
 }
--- newsapp/src/Components/NewsItem.js
+++ newsapp/src/Components/NewsItem.js
@@ -3,20 +3,21 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   constructor(){
     super();
-    console.log("what's up") 
+    // console.log("what's up") 
   }
 
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;// destructuring
+    let {title,description,imageUrl,newsUrl,author,published} = this.props;// destructuring,passed from News.js
     
     return (
       <div className="my-3">
-         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+         <div className="card">
+          <img src={imageUrl?imageUrl:"https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/VAUQYVDG4FEJZAV4VMOFG2IIEM.jpg&w=1440"} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}<span class="badge bg-secondary">New</span></h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+            <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {published}</small></p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
           </div>
       </div>
       </div>
